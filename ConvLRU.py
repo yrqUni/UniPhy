5c32818f881b72bdf1b9f4aad9796f840c64a03a
RuiqingYan <yrquni@RuiqingYandeMacBook-Pro.local>
2024-05-08 00:54:52 +1000

    v0.0.1

--- ConvLRU.py
+++ ConvLRU.py
@@ -113,7 +113,8 @@ class ConvLRULayer(nn.Module):
         gamma_log = torch.log(torch.sqrt(1 - torch.abs(diag_lambda) ** 2))
         self.params_log = nn.Parameter(torch.vstack((nu_log, theta_log, gamma_log)))
         # define layers
-        self.in_proj = nn.Conv2d(self.emb_ch, self.hidden_ch, kernel_size=1, padding='same', bias=use_bias).to(torch.cfloat)
+        self.in_proj_B = nn.Conv2d(self.emb_ch, self.hidden_ch, kernel_size=1, padding='same', bias=use_bias).to(torch.cfloat)
+        self.in_proj_P = nn.Conv2d(self.emb_ch, self.hidden_ch, kernel_size=1, padding='same', bias=use_bias).to(torch.cfloat)
         self.out_proj = nn.Conv2d(self.hidden_ch, self.emb_ch, kernel_size=1, padding='same', bias=use_bias).to(torch.cfloat)
         self.dropout = nn.Dropout(p=dropout)
         self.layer_norm = nn.LayerNorm([self.emb_ch, self.input_size, self.input_size])
@@ -133,13 +134,17 @@ class ConvLRULayer(nn.Module):
         B, L, _, H, W = x.size()
         nu, theta, gamma = torch.exp(self.params_log).split((self.hidden_ch, self.hidden_ch, self.hidden_ch))
         lamb = torch.exp(torch.complex(-nu, theta))
-        h = self.in_proj(x.reshape(B*L, self.emb_ch, H, W).to(torch.cfloat)).reshape(B, L, self.hidden_ch, H, W)
-        h = torch.fft.ifft2(h) * torch.diag_embed(gamma)
+        h = self.in_proj_B(x.reshape(B*L, self.emb_ch, H, W).to(torch.cfloat)).reshape(B, L, self.hidden_ch, H, W)
+        h = torch.fft.fft2(h)
+        h = self.in_proj_P(x.reshape(B*L, self.emb_ch, H, W).to(torch.cfloat)).reshape(B, L, self.hidden_ch, H, W)
+        h = h * torch.diag_embed(gamma)
         log2_L = int(np.ceil(np.log2(L)))
         for i in range(log2_L):
             h, lamb = self.lru_parallel(i + 1, h, lamb, B, L,  self.hidden_ch, H, W)
         x_ = self.out_proj(h.reshape(B*L, self.hidden_ch, H, W )).reshape(B, L, self.emb_ch, H, W).real
         x_ = self.dropout(x_)
+        x_ = torch.fft.ifft2(x_)
+        x_ = x_.real
         x_ = self.layer_norm(x_.reshape(B*L, self.emb_ch, H, W )).reshape(B, L, self.emb_ch, H, W)
         x = x_ + x
         return x
--- TryConvLRU.py
+++ TryConvLRU.py
@@ -10,12 +10,12 @@ class Args:
     ffn_dropout = 0.1
     convlru_num_blocks = 12
 args = Args()
-model = ConvLRU(args)
+model = ConvLRU(args).cuda()
 loss_fn = torch.nn.MSELoss()
 opt = torch.optim.Adam(model.parameters(), lr=0.001)
 opt.zero_grad()
-input = torch.randn(8, 32, 3, 100, 100)
-label = torch.randn(8, 32, 3, 100, 100)
+input = torch.randn(4, 32, 3, 100, 100).cuda()
+label = torch.randn(4, 32, 3, 100, 100).cuda()
 outputs = model(input)
 loss = loss_fn(outputs, label)
 loss.backward()
