b9863bd25b7e59dc79d0e35641b0b2e71cd6bc1d
yrqUni <yrqUni@gmail.com>
2024-05-16 13:24:28 +0000

    v0.0.11

--- Demo/TryConvLRU.py
+++ Demo/TryConvLRU.py
@@ -3,18 +3,18 @@ import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 import torch
-from model.ConvLRU import ConvLRU
+from Model.ConvLRU import ConvLRU
 
 class Args:
     # input info
-    input_size = 32
+    input_size = (300, 200)
     input_ch = 1
     # convlru info
     convlru_hidden_ch = 8
     convlru_dropout = 0.1  
     convlru_num_blocks = 12
     #
-    hidden_factor = 2
+    hidden_factor = (3, 2)
     # emb info
     emb_ch = 4
     emb_hidden_ch = 8
@@ -34,8 +34,8 @@ L = 8
 model = ConvLRU(args).cuda()
 loss_fn = torch.nn.MSELoss()
 opt = torch.optim.Adam(model.parameters(), lr=0.001)
-inputs_train = torch.randn(B, L, args.input_ch, args.input_size, args.input_size).cuda()
-labels_train = torch.randn(B, L, args.input_ch, args.input_size, args.input_size).cuda()
+inputs_train = torch.randn(B, L, args.input_ch, *args.input_size).cuda()
+labels_train = torch.randn(B, L, args.input_ch, *args.input_size).cuda()
 opt.zero_grad()
 outputs = model(inputs_train, mask=None, mode='train', out_frames=None)
 loss = loss_fn(outputs, labels_train)
@@ -43,9 +43,9 @@ loss.backward()
 opt.step()
 print(f"Train mode Loss {loss}")
 out_frames = 4
-inputs_train = torch.randn(B, L, args.input_ch, args.input_size, args.input_size).cuda()
+inputs_train = torch.randn(B, L, args.input_ch, *args.input_size).cuda()
 mask = (torch.randn(B, L).cuda() > 0).float() 
-labels_train = torch.randn(B, out_frames, args.input_ch, args.input_size, args.input_size).cuda()
+labels_train = torch.randn(B, out_frames, args.input_ch, *args.input_size).cuda()
 opt.zero_grad()
 outputs = model(inputs_train, mode='infer', out_frames=out_frames)
 loss = loss_fn(outputs, labels_train)
--- Demo/TryConvLSTM.py
+++ Demo/TryConvLSTM.py
@@ -5,7 +5,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from model.ConvLSTM import ConvLSTM
+from Model.ConvLSTM import ConvLSTM
 
 batch_size = 10
 seq_length = 5
