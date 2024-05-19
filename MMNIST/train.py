e0731cfec3292fa31efa0ee793413e1258fa7f75
yrqUni <yrqUni@gmail.com>
2024-05-19 05:35:23 +0000

    v0.1.2

--- MMNIST/train.py
+++ MMNIST/train.py
@@ -1,5 +1,7 @@
 import sys
 import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -38,10 +40,12 @@ class Args:
     n_frames_input = 17
     n_frames_output = 1
     num_objects = [3]
+    num_samples = int(5e3)
     # training info
-    batch_size = 16
+    batch_size = 8
     lr = 1e-4
-    num_steps = 400000
+    EPs = 500
+    vis = 100
     log_file = f'log_{convlru_num_blocks}'
     save_path = f'ckpt_{convlru_num_blocks}/'
     pretrain_path = './ckpt_6/A.pth'
@@ -52,9 +56,8 @@ if not os.path.exists(args.save_path):
 
 logging.basicConfig(filename=args.log_file, level=logging.INFO)
 
-dataset = MovingMNIST(root=args.root, is_train=args.is_train, n_frames_input=args.n_frames_input, n_frames_output=args.n_frames_output, num_objects=args.num_objects, num_samples=int(1e10))
+dataset = MovingMNIST(root=args.root, is_train=args.is_train, n_frames_input=args.n_frames_input, n_frames_output=args.n_frames_output, num_objects=args.num_objects, num_samples=args.num_samples)
 dataloader = DataLoader(dataset, batch_size=args.batch_size, shuffle=False, num_workers=4, pin_memory=True, prefetch_factor=2)
-data_iter = iter(dataloader)
 
 model = ConvLRU(args).cuda()
 
@@ -66,25 +69,28 @@ else:
 
 loss_fn = nn.CrossEntropyLoss()
 opt = optim.AdamW(model.parameters(), lr=args.lr)
-scheduler = optim.lr_scheduler.CosineAnnealingLR(opt, T_max=args.num_steps)
+scheduler = optim.lr_scheduler.CosineAnnealingLR(opt, T_max=len(dataloader))
 
-for step in tqdm(range(args.num_steps)):
-    try:
-        inputs, _ = next(data_iter)
-    except StopIteration:
-        data_iter = iter(dataloader)
-        inputs, _ = next(data_iter)
+for ep in range(args.EPs):
+    model.train()
+    running_loss = 0.0
+    for step, (inputs, _) in enumerate(tqdm(dataloader)):
+        inputs = inputs.cuda()
+        opt.zero_grad()
+        pred_outputs = model(inputs[:, :-1], mode='p')
+        loss = loss_fn(pred_outputs, inputs[:, 1:])
+        loss.backward()
+        opt.step()
+        scheduler.step()
+        
+        running_loss += loss.item()
+        
+        if (step + 1) % args.vis == 0:
+            avg_loss = running_loss / args.vis
+            current_lr = scheduler.get_last_lr()[0]
+            logging.info(f'Step {step+1}, Epoch {ep}, Average Loss: {avg_loss}, LR: {current_lr}')
+            torch.save(model.state_dict(), os.path.join(args.save_path, f'{ep}_{step+1}.pth'))
+            tqdm.write(f'Step {step+1}, Epoch {ep}, Average Loss: {avg_loss}, LR: {current_lr}')
+            running_loss = 0.0
 
-    inputs = inputs.cuda()
-    opt.zero_grad()
-    pred_outputs = model(inputs[:, :-1], mode='p')
-    loss = loss_fn(pred_outputs, inputs[:, 1:])
-    loss.backward()
-    opt.step()
-    scheduler.step()
-    if step % 100 == 0:
-        current_lr = scheduler.get_last_lr()[0]
-        logging.info(f'Step {step}, Loss: {loss.item()}, LR: {current_lr}')
-        torch.save(model.state_dict(), os.path.join(args.save_path, f'{step}.pth'))
-    tqdm.write(f'Step {step}, Loss: {loss.item()}, LR: {current_lr}')
 logging.shutdown()
