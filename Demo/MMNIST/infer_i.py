dd7536688b6f8a31e787af1d54f7858a5638bc4f
yrqUni <yrqUni@gmail.com>
2024-05-25 06:47:01 +0000

    v0.1.16b

--- Demo/MMNIST/infer_i.py
+++ Demo/MMNIST/infer_i.py
@@ -26,33 +26,33 @@ class Args:
         #
         self.hidden_factor = (2, 2)
         # emb info
-        self.emb_hidden_ch = 128
+        self.emb_hidden_ch = 64
         self.emb_dropout = 0.0
         self.emb_hidden_layers_num = 4
         # ffn info
-        self.ffn_hidden_ch = 128
+        self.ffn_hidden_ch = 32
         self.ffn_dropout = 0.0
         self.ffn_hidden_layers_num = 2
         # dec info
-        self.dec_hidden_ch = 128
+        self.dec_hidden_ch = 64
         self.dec_dropout = 0.0
         self.dec_hidden_layers_num = 4
         # data info
         self.root = './DATA/MMNIST/'
         self.is_train = True
-        self.n_frames_input = 16
-        self.n_frames_output = 16
-        self.num_objects = [3]
+        self.n_frames_input = 128
+        self.n_frames_output = 128
+        self.num_objects = [2]
         self.num_samples = int(5e3)
         # training info
-        self.batch_size = 16
+        self.batch_size = 2
         self.lr = 1e-3
         self.EPs = 500
-        self.vis = 1
-        self.out_path = './exp6/'
+        self.vis_num = 16
+        self.out_path = './exp_b/'
         self.log_file = os.path.join(self.out_path, 'log')
         self.vis_path = os.path.join(self.out_path, 'vis/')
-        self.pretrain_path = '/data1/ruiqingy/Workspace/ConvLRU/Demo/MMNIST/exp2/ckpt/A.pth'
+        self.pretrain_path = '/data1/ruiqingy/Workspace/ConvLRU/Demo/MMNIST/exp1/ckpt/A.pth'
     def __str__(self):
         attrs = vars(self)
         return '\n'.join(f'{k}: {v}' for k, v in attrs.items())
@@ -80,19 +80,20 @@ else:
 
 loss_fn = nn.BCELoss().cuda()
 
-def visualize(GTs, PREDs, step):
+def visualize(GTs, PREDs, step, vis_num, sample_idx):
     L = GTs.size(1)
-    GTs, PREDs = GTs.squeeze(2), PREDs.squeeze(2)
-    _, axes = plt.subplots(2, L, figsize=(20, 5))
-    for i in range(L):
-        axes[0, i].imshow(GTs[0, i].cpu().numpy(), cmap='gray')
-        axes[0, i].set_title(f"GT {i}")
+    indices = torch.linspace(0, L - 1, steps=vis_num).long()
+    GTs, PREDs = GTs[:, indices], PREDs[:, indices]
+    _, axes = plt.subplots(2, vis_num, figsize=(20, 5))
+    for i in range(vis_num):
+        axes[0, i].imshow(GTs[0, i].cpu().numpy().squeeze(), cmap='gray')
+        axes[0, i].set_title(f"GT {indices[i].item()}")
         axes[0, i].axis('off')
-        axes[1, i].imshow(PREDs[0, i].cpu().detach().numpy(), cmap='gray')
-        axes[1, i].set_title(f"Pred {i}")
+        axes[1, i].imshow(PREDs[0, i].cpu().detach().numpy().squeeze(), cmap='gray')
+        axes[1, i].set_title(f"Pred {indices[i].item()}")
         axes[1, i].axis('off')
     plt.tight_layout()
-    plt.savefig(os.path.join(args.vis_path, f'out_{step}.png'))
+    plt.savefig(os.path.join(args.vis_path, f'out_{step}_sample_{sample_idx}.png'))
     plt.close()
 
 model.eval()
@@ -103,11 +104,10 @@ with torch.no_grad():
         pred_outputs = model(inputs, mode='i_sigmoid', out_frames_num=args.n_frames_output)
         loss = loss_fn(pred_outputs, outputs)
         running_loss += loss.item()
-        if (step + 1) % args.vis == 0:
-            avg_loss = running_loss / args.vis
-            logging.info(f'Step {step+1}, Average Loss: {avg_loss}')
-            tqdm.write(f'Step {step+1}, Average Loss: {avg_loss}')
-            running_loss = 0.0
-            visualize(outputs, pred_outputs, step + 1)
+        logging.info(f'Step {step+1}, Average Loss: {running_loss}')
+        tqdm.write(f'Step {step+1}, Average Loss: {running_loss}')
+        running_loss = 0.0
+        for sample_idx in range(outputs.size(0)):
+            visualize(outputs[sample_idx:sample_idx+1], pred_outputs[sample_idx:sample_idx+1], step + 1, args.vis_num, sample_idx)
 
 logging.shutdown()
