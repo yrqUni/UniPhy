085b8b1b6491843421fa9cd3536ed4d6ee78e4cd
Ruiqing Yan <z5494660@L-JFQJ2K37HH.local>
2025-03-31 14:09:08 +0800

    v0.11.5

--- Model/ConvLRU/pscan.py
+++ Model/ConvLRU/pscan.py
@@ -2,215 +2,221 @@ import math
 import torch
 import torch.nn.functional as F
 
-def NextPowerOf2(sequence_length):
-    """Returns smallest power of 2 >= sequence_length"""
-    return 2 ** math.ceil(math.log2(sequence_length))
+def npo2(len):
+    """
+    Returns the next power of 2 above len.
+    """
+    return 2 ** math.ceil(math.log2(len))
 
-def PadNextPowerOf2(input_tensor):
-    """Pads tensor's length dim to next power of 2"""
-    target_length = NextPowerOf2(input_tensor.size(1))
-    pad_tuple = (0, 0, 0, 0, 0, 0, 0, target_length - input_tensor.size(1))
-    return F.pad(input_tensor, pad_tuple, "constant", 0)
+def pad_npo2(X):
+    """
+    Pads input length dim to the next power of 2.
+    Args:
+        X : (B, L, C, S, S)
+    Returns:
+        Y : (B, npo2(L), C, S, S)
+    """
+    len_npo2 = npo2(X.size(1))
+    pad_tuple = (0, 0, 0, 0, 0, 0, 0, len_npo2 - X.size(1))
+    return F.pad(X, pad_tuple, "constant", 0)
 
 class PScan(torch.autograd.Function):
-    """
-    Parallel Scan algorithm implementation.
-    
-    Efficient O(log n) algorithm for recurrence H_t = A_t * H_{t-1} + X_t,
-    versus O(n) for sequential computation.
-    """
-    
     @staticmethod
     def pscan(A, X):
         """
-        Core parallel scan forward implementation.
-        
-        Args:
-            A: Coefficient tensor (batch_size, seq_length, channels, state_dim, 1)
-            X: Input tensor (batch_size, seq_length, channels, state_dim, state_dim)
+        A : (B, L, C, S, 1)
+        X : (B, L, C, S, S)
         """
-        batch_size, seq_length, channels, state_dim, _ = A.size()
-        num_steps = int(math.log2(seq_length))
+        B, L, C, S, _ = A.size()
+        num_steps = int(math.log2(L))
 
-        # Initialize working tensors
         Aa = A
         Xa = X
-        
-        # Up-sweep phase
         for _ in range(num_steps - 2):
-            current_length = Xa.size(1)
-            Aa = Aa.view(batch_size, current_length // 2, 2, channels, state_dim, 1)
-            Xa = Xa.view(batch_size, current_length // 2, 2, channels, state_dim, state_dim)
+            T = Xa.size(1)
+            Aa = Aa.view(B, T // 2, 2, C, S, 1)
+            Xa = Xa.view(B, T // 2, 2, C, S, S)
             
-            product = Aa[:, :, 1].clone() * Xa[:, :, 0].clone()
-            Xa[:, :, 1] = Xa[:, :, 1].clone() + product
-            Aa[:, :, 1] = Aa[:, :, 1].clone() * Aa[:, :, 0].clone()
+            Xa[:, :, 1].add_(Aa[:, :, 1].mul(Xa[:, :, 0]))
+            Aa[:, :, 1].mul_(Aa[:, :, 0])
 
             Aa = Aa[:, :, 1]
             Xa = Xa[:, :, 1]
 
-        # Special handling for length 4 case
         if Xa.size(1) == 4:
-            product1 = Aa[:, 1].clone() * Xa[:, 0].clone()
-            Xa[:, 1] = Xa[:, 1].clone() + product1
-            Aa[:, 1] = Aa[:, 1].clone() * Aa[:, 0].clone()
+            Xa[:, 1].add_(Aa[:, 1].mul(Xa[:, 0]))
+            Aa[:, 1].mul_(Aa[:, 0])
 
-            product2 = Aa[:, 2].clone() * Xa[:, 1].clone()
-            product3 = Aa[:, 3].clone() * (Xa[:, 2].clone() + product2)
-            Xa[:, 3] = Xa[:, 3].clone() + product3
-        # Special handling for length 2 case
+            Xa[:, 3].add_(Aa[:, 3].mul(Xa[:, 2] + Aa[:, 2].mul(Xa[:, 1])))
         elif Xa.size(1) == 2:
-            product = Aa[:, 1].clone() * Xa[:, 0].clone()
-            Xa[:, 1] = Xa[:, 1].clone() + product
+            Xa[:, 1].add_(Aa[:, 1].mul(Xa[:, 0]))
             return
         else:
             return
 
-        # Down-sweep phase
-        Aa = A[:, 2 ** (num_steps - 2) - 1:seq_length:2 ** (num_steps - 2)]
-        Xa = X[:, 2 ** (num_steps - 2) - 1:seq_length:2 ** (num_steps - 2)]
-        
-        product = Aa[:, 2].clone() * Xa[:, 1].clone()
-        Xa[:, 2] = Xa[:, 2].clone() + product
-        Aa[:, 2] = Aa[:, 2].clone() * Aa[:, 1].clone()
+        Aa = A[:, 2 ** (num_steps - 2) - 1:L:2 ** (num_steps - 2)]
+        Xa = X[:, 2 ** (num_steps - 2) - 1:L:2 ** (num_steps - 2)]
+        Xa[:, 2].add_(Aa[:, 2].mul(Xa[:, 1]))
+        Aa[:, 2].mul_(Aa[:, 1])
 
         for k in range(num_steps - 3, -1, -1):
-            Aa = A[:, 2 ** k - 1:seq_length:2 ** k]
-            Xa = X[:, 2 ** k - 1:seq_length:2 ** k]
+            Aa = A[:, 2 ** k - 1:L:2 ** k]
+            Xa = X[:, 2 ** k - 1:L:2 ** k]
 
-            current_length = Xa.size(1)
-            Aa = Aa.view(batch_size, current_length // 2, 2, channels, state_dim, 1)
-            Xa = Xa.view(batch_size, current_length // 2, 2, channels, state_dim, state_dim)
+            T = Xa.size(1)
+            Aa = Aa.view(B, T // 2, 2, C, S, 1)
+            Xa = Xa.view(B, T // 2, 2, C, S, S)
             
-            product = Aa[:, 1:, 0].clone() * Xa[:, :-1, 1].clone()
-            Xa[:, 1:, 0] = Xa[:, 1:, 0].clone() + product
-            Aa[:, 1:, 0] = Aa[:, 1:, 0].clone() * Aa[:, :-1, 1].clone()
+            Xa[:, 1:, 0].add_(Aa[:, 1:, 0].mul(Xa[:, :-1, 1]))
+            Aa[:, 1:, 0].mul_(Aa[:, :-1, 1])
 
     @staticmethod
     def pscan_rev(A, X):
         """
-        Reverse version of parallel scan (for backpropagation).
-        
-        Args:
-            A: Coefficient tensor (batch_size, seq_length, channels, state_dim, 1)
-            X: Input tensor (batch_size, seq_length, channels, state_dim, state_dim)
+        A : (B, L, C, S, 1)
+        X : (B, L, C, S, S)
         """
-        batch_size, seq_length, channels, state_dim, _ = A.size()
-        num_steps = int(math.log2(seq_length))
+        B, L, C, S, _ = A.size()
+        num_steps = int(math.log2(L))
 
         Aa = A
         Xa = X
-        
-        # Up-sweep phase (reverse)
         for _ in range(num_steps - 2):
-            current_length = Xa.size(1)
-            Aa = Aa.view(batch_size, current_length // 2, 2, channels, state_dim, 1)
-            Xa = Xa.view(batch_size, current_length // 2, 2, channels, state_dim, state_dim)
-            
-            product = Aa[:, :, 0].clone() * Xa[:, :, 1].clone()
-            Xa[:, :, 0] = Xa[:, :, 0].clone() + product
-            Aa[:, :, 0] = Aa[:, :, 0].clone() * Aa[:, :, 1].clone()
+            T = Xa.size(1)
+            Aa = Aa.view(B, T // 2, 2, C, S, 1)
+            Xa = Xa.view(B, T // 2, 2, C, S, S)
+                    
+            Xa[:, :, 0].add_(Aa[:, :, 0].mul(Xa[:, :, 1]))
+            Aa[:, :, 0].mul_(Aa[:, :, 1])
 
             Aa = Aa[:, :, 0]
             Xa = Xa[:, :, 0]
 
-        # Special handling for length 4 case
         if Xa.size(1) == 4:
-            product1 = Aa[:, 2].clone() * Xa[:, 3].clone()
-            Xa[:, 2] = Xa[:, 2].clone() + product1
-            Aa[:, 2] = Aa[:, 2].clone() * Aa[:, 3].clone()
+            Xa[:, 2].add_(Aa[:, 2].mul(Xa[:, 3]))
+            Aa[:, 2].mul_(Aa[:, 3])
 
-            product2 = Aa[:, 1].clone() * Xa[:, 2].clone()
-            product3 = Aa[:, 0].clone() * (Xa[:, 1].clone() + product2)
-            Xa[:, 0] = Xa[:, 0].clone() + product3
-        # Special handling for length 2 case
+            Xa[:, 0].add_(Aa[:, 0].mul(Xa[:, 1].add(Aa[:, 1].mul(Xa[:, 2]))))
         elif Xa.size(1) == 2:
-            product = Aa[:, 0].clone() * Xa[:, 1].clone()
-            Xa[:, 0] = Xa[:, 0].clone() + product
+            Xa[:, 0].add_(Aa[:, 0].mul(Xa[:, 1]))
             return
         else:
             return
 
-        # Down-sweep phase (reverse)
-        Aa = A[:, 0:seq_length:2 ** (num_steps - 2)]
-        Xa = X[:, 0:seq_length:2 ** (num_steps - 2)]
-        
-        product = Aa[:, 1].clone() * Xa[:, 2].clone()
-        Xa[:, 1] = Xa[:, 1].clone() + product
-        Aa[:, 1] = Aa[:, 1].clone() * Aa[:, 2].clone()
+        Aa = A[:, 0:L:2 ** (num_steps - 2)]
+        Xa = X[:, 0:L:2 ** (num_steps - 2)]
+        Xa[:, 1].add_(Aa[:, 1].mul(Xa[:, 2]))
+        Aa[:, 1].mul_(Aa[:, 2])
 
         for k in range(num_steps - 3, -1, -1):
-            Aa = A[:, 0:seq_length:2 ** k]
-            Xa = X[:, 0:seq_length:2 ** k]
+            Aa = A[:, 0:L:2 ** k]
+            Xa = X[:, 0:L:2 ** k]
 
-            current_length = Xa.size(1)
-            Aa = Aa.view(batch_size, current_length // 2, 2, channels, state_dim, 1)
-            Xa = Xa.view(batch_size, current_length // 2, 2, channels, state_dim, state_dim)
-            
-            product = Aa[:, :-1, 1].clone() * Xa[:, 1:, 0].clone()
-            Xa[:, :-1, 1] = Xa[:, :-1, 1].clone() + product
-            Aa[:, :-1, 1] = Aa[:, :-1, 1].clone() * Aa[:, 1:, 0].clone()
+            T = Xa.size(1)
+            Aa = Aa.view(B, T // 2, 2, C, S, 1)
+            Xa = Xa.view(B, T // 2, 2, C, S, S)
+
+            Xa[:, :-1, 1].add_(Aa[:, :-1, 1].mul(Xa[:, 1:, 0]))
+            Aa[:, :-1, 1].mul_(Aa[:, 1:, 0])
 
     @staticmethod
     def forward(ctx, A_in, X):
         """
-        Forward pass function for parallel scan.
-        
+        Applies the parallel scan operation, as defined above. 
+        Returns a new tensor.
+        Privilege sequence lengths that are powers of two.
         Args:
-            ctx: Context object for backward pass
-            A_in: Coefficient tensor (batch_size, seq_length, channels, state_dim, 1)
-            X: Input tensor (batch_size, seq_length, channels, state_dim, state_dim)
-            
+            A_in : (B, L, C, S, 1)
+            X : (B, L, C, S, S)
         Returns:
-            Parallel scan result with original sequence length
+            H : (B, L, C, S, S)
         """
-        original_length = X.size(1)
-        
-        # Check if padding is needed
-        if original_length == NextPowerOf2(original_length):
+        L = X.size(1)
+
+        if L == npo2(L):
             A = A_in.clone()
-            X_work = X.clone()
+            X = X.clone()
         else:
-            A = PadNextPowerOf2(A_in)
-            X_work = PadNextPowerOf2(X)
+            A = pad_npo2(A_in) # (B, npo2(L), C, S, S)
+            X = pad_npo2(X) # (B, npo2(L), C, S, S)
         
-        PScan.pscan(A, X_work)
-        ctx.save_for_backward(A_in, X_work)
+        PScan.pscan(A, X)
+
+        ctx.save_for_backward(A_in, X)
         
-        return X_work[:, :original_length]
+        return X[:, :L]
     
     @staticmethod
     def backward(ctx, grad_output_in):
         """
-        Backward pass function computing gradients for A and X.
-        
+        Flows the gradient from the output to the input. 
+        Returns two new tensors.
         Args:
-            ctx: Context from forward pass
-            grad_output_in: Output gradient
-            
+            ctx : A : (B, L, C, S, 1), X : (B, L, C, S, S)
+            grad_output_in : (B, L, C, S, S)
         Returns:
-            Gradients for A_in and X
+            gradA : (B, L, C, S, S), gradX : (B, L, C, S, S)
         """
         A, X = ctx.saved_tensors
-        original_length = grad_output_in.size(1)
-        
-        # Handle padding for gradients
-        if original_length == NextPowerOf2(original_length):
+
+        L = grad_output_in.size(1)
+
+        if L == npo2(L):
             grad_output = grad_output_in.clone()
         else:
-            grad_output = PadNextPowerOf2(grad_output_in)
-            A = PadNextPowerOf2(A)
+            grad_output = pad_npo2(grad_output_in) # (B, npo2(L), C, S, S)
+            A = pad_npo2(A) # (B, npo2(L), C, S, 1)
 
-        # Prepare for reverse scan
-        A_padded = torch.nn.functional.pad(A[:, 1:], (0, 0, 0, 0, 0, 0, 0, 1))
-        PScan.pscan_rev(A_padded, grad_output)
+        A = torch.nn.functional.pad(A[:, 1:], (0, 0, 0, 0, 0, 0, 0, 1))
 
-        # Compute gradient for A
+        PScan.pscan_rev(A, grad_output)
+
         Q = torch.zeros_like(X)
-        for i in range(1, X.size(1)):
-            Q[:, i] = X[:, i-1].clone() * grad_output[:, i].clone()
+        Q[:, 1:].add_(X[:, :-1] * grad_output[:, 1:])
 
-        return Q[:, :original_length], grad_output[:, :original_length]
+        return Q[:, :L], grad_output[:, :L]
+    
+pscan = PScan.apply
+
+def serial_scan(A, X):
+    """
+    Serial implementation of the scan operation.
+    Args:
+        A : (B, L, C, S, 1)
+        X : (B, L, C, S, S)
+    Returns:
+        H : (B, L, C, S, S)
+    """
+    B, L, C, S, S = A.size()
+    H = torch.zeros_like(X)
+    for b in range(B):
+        for l in range(L):
+            if l == 0:
+                H[b, l] = X[b, l].clone()
+            else:
+                H[b, l] = A[b, l] * H[b, l - 1].clone() + X[b, l].clone()
+    return H
+
+def pscan_check(B=2, L=13, C=8, S=16):
+    """
+    Checks the parallel scan implementation.
+    """
+    _A = torch.rand(B, L, C, S, 1)
+    A1 = torch.nn.Parameter(_A.clone())
+    A2 = torch.nn.Parameter(_A.clone())
+    X1 = torch.rand(B, L, C, S, S)
+    X2 = X1.clone()
+    H_gt = torch.rand(B, L, C, S, S)
+
+    torch.autograd.set_detect_anomaly(True)
+    loss_fn = torch.nn.MSELoss()
+    H_pscan = pscan(A1.expand(B, L, C, S, 1), X1)
+    loss_pscan = loss_fn(H_pscan, H_gt)
+    loss_pscan.backward()
+    H_serial_scan = serial_scan(A2.expand(B, L, C, S, 1), X2)
+    loss_serial_scan = loss_fn(H_serial_scan, H_gt)
+    loss_serial_scan.backward()
+    return torch.allclose(H_pscan, H_serial_scan), torch.allclose(A1.grad, A2.grad)
 
 def serial_scan(A, X):
     """
