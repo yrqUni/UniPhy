c7b5e2842ca21579604e41b10513a8be9df3f094
RuiqingYan <yrqUni@gmail.com>
2026-01-15 09:29:15 +0000

    uniphy

--- Model/UniPhy/GrandUnified.py
+++ Model/UniPhy/GrandUnified.py
@@ -1,51 +1,207 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from GrandUnifiedTritonOps import FusedHamiltonian, fused_curl_2d
+import triton
+import triton.language as tl
+
+@triton.jit
+def fused_hamiltonian_kernel(
+    z_real_ptr, z_imag_ptr,
+    h_real_ptr, h_imag_ptr,
+    noise_real_ptr, noise_imag_ptr,
+    dt_ptr,
+    n_elements,
+    sample_size,
+    sigma,
+    BLOCK_SIZE: tl.constexpr
+):
+    pid = tl.program_id(axis=0)
+    block_start = pid * BLOCK_SIZE
+    offsets = block_start + tl.arange(0, BLOCK_SIZE)
+    mask = offsets < n_elements
+
+    batch_idx = offsets // sample_size
+    
+    dt = tl.load(dt_ptr + batch_idx, mask=mask, other=0.0)
+
+    z_r = tl.load(z_real_ptr + offsets, mask=mask)
+    z_i = tl.load(z_imag_ptr + offsets, mask=mask)
+    h_r = tl.load(h_real_ptr + offsets, mask=mask)
+    h_i = tl.load(h_imag_ptr + offsets, mask=mask)
+    
+    real_arg = -h_i * dt
+    imag_arg = h_r * dt
+    
+    mag = tl.exp(real_arg)
+    cos_v = tl.cos(imag_arg)
+    sin_v = tl.sin(imag_arg)
+    
+    prop_r = mag * cos_v
+    prop_i = mag * sin_v
+    
+    out_r = z_r * prop_r - z_i * prop_i
+    out_i = z_r * prop_i + z_i * prop_r
+    
+    noise_r = tl.load(noise_real_ptr + offsets, mask=mask)
+    noise_i = tl.load(noise_imag_ptr + offsets, mask=mask)
+    
+    scale = sigma * tl.sqrt(dt)
+    
+    out_r = out_r + scale * noise_r
+    out_i = out_i + scale * noise_i
+    
+    tl.store(z_real_ptr + offsets, out_r, mask=mask)
+    tl.store(z_imag_ptr + offsets, out_i, mask=mask)
+
+class FusedHamiltonian(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, z_real, z_imag, h_real, h_imag, dt, sigma_tensor):
+        sigma_val = sigma_tensor.item()
+        
+        noise_real = torch.randn_like(z_real)
+        noise_imag = torch.randn_like(z_imag)
+        
+        ctx.save_for_backward(z_real, z_imag, h_real, h_imag, noise_real, noise_imag, dt)
+        ctx.sigma_val = sigma_val
+        
+        n_elements = z_real.numel()
+        B = z_real.shape[0]
+        sample_size = n_elements // B
+        
+        out_real = z_real.clone()
+        out_imag = z_imag.clone()
+        
+        grid = lambda meta: (triton.cdiv(n_elements, meta['BLOCK_SIZE']),)
+        
+        dt_flat = dt.view(-1).contiguous()
+
+        fused_hamiltonian_kernel[grid](
+            out_real, out_imag,
+            h_real, h_imag,
+            noise_real, noise_imag,
+            dt_flat,
+            n_elements, 
+            sample_size,
+            float(sigma_val),
+            BLOCK_SIZE=1024
+        )
+        
+        return out_real, out_imag
+
+    @staticmethod
+    def backward(ctx, grad_out_real, grad_out_imag):
+        z_r, z_i, h_r, h_i, n_r, n_i, dt = ctx.saved_tensors
+        
+        dt_expanded = dt.view(-1, 1, 1, 1)
+        
+        H = torch.complex(h_r, h_i)
+        prop = torch.exp(1j * H * dt_expanded)
+        
+        grad_out = torch.complex(grad_out_real, grad_out_imag)
+        grad_z = grad_out * torch.conj(prop)
+        
+        z_in = torch.complex(z_r, z_i)
+        z_out_det = z_in * prop
+        grad_H = grad_out * torch.conj(z_out_det) * 1j * dt_expanded
+        
+        sqrt_dt = torch.sqrt(dt_expanded)
+        grad_sigma = torch.sum(grad_out_real * (sqrt_dt * n_r) + grad_out_imag * (sqrt_dt * n_i))
+        
+        return grad_z.real, grad_z.imag, grad_H.real, grad_H.imag, None, grad_sigma
+
+@triton.jit
+def stencil_curl_kernel(
+    psi_ptr,
+    u_ptr, v_ptr,
+    stride_b, stride_c, stride_h, stride_w,
+    H, W,
+    BLOCK_H: tl.constexpr, BLOCK_W: tl.constexpr
+):
+    pid_h = tl.program_id(0)
+    pid_w = tl.program_id(1)
+    
+    off_h = pid_h * BLOCK_H + tl.arange(0, BLOCK_H)
+    off_w = pid_w * BLOCK_W + tl.arange(0, BLOCK_W)
+    
+    mask_h = off_h < H
+    mask_w = off_w < W
+    
+    idx_up = tl.maximum(off_h - 1, 0)
+    idx_down = tl.minimum(off_h + 1, H - 1)
+    idx_left = tl.maximum(off_w - 1, 0)
+    idx_right = tl.minimum(off_w + 1, W - 1)
+    
+    base_ptr = psi_ptr
+    
+    ptr_up = base_ptr + (idx_up[:, None] * stride_h + off_w[None, :] * stride_w)
+    ptr_down = base_ptr + (idx_down[:, None] * stride_h + off_w[None, :] * stride_w)
+    ptr_left = base_ptr + (off_h[:, None] * stride_h + idx_left[None, :] * stride_w)
+    ptr_right = base_ptr + (off_h[:, None] * stride_h + idx_right[None, :] * stride_w)
+    
+    val_up = tl.load(ptr_up, mask=mask_h[:, None] & mask_w[None, :], other=0.0)
+    val_down = tl.load(ptr_down, mask=mask_h[:, None] & mask_w[None, :], other=0.0)
+    val_left = tl.load(ptr_left, mask=mask_h[:, None] & mask_w[None, :], other=0.0)
+    val_right = tl.load(ptr_right, mask=mask_h[:, None] & mask_w[None, :], other=0.0)
+    
+    grad_y = (val_down - val_up) * 0.5
+    grad_x = (val_right - val_left) * 0.5
+    
+    offset_out = off_h[:, None] * stride_h + off_w[None, :] * stride_w
+    tl.store(u_ptr + offset_out, grad_y, mask=mask_h[:, None] & mask_w[None, :])
+    tl.store(v_ptr + offset_out, -grad_x, mask=mask_h[:, None] & mask_w[None, :])
+
+def fused_curl_2d(psi):
+    psi = psi.contiguous()
+    B, C, H, W = psi.shape
+    u = torch.empty_like(psi)
+    v = torch.empty_like(psi)
+    
+    total_slices = B * C
+    psi_flat = psi.view(total_slices, H, W)
+    u_flat = u.view(total_slices, H, W)
+    v_flat = v.view(total_slices, H, W)
+    
+    grid = lambda meta: (triton.cdiv(H, meta['BLOCK_H']), triton.cdiv(W, meta['BLOCK_W']))
+    
+    for i in range(total_slices):
+        stencil_curl_kernel[grid](
+            psi_flat[i], u_flat[i], v_flat[i],
+            0, 0, psi_flat.stride(1), psi_flat.stride(2),
+            H, W,
+            BLOCK_H=16, BLOCK_W=16
+        )
+    return u, v
 
 class CliffordConv2d(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size, padding=0):
+    def __init__(self, dim, kernel_size=3, padding=1):
         super().__init__()
-        self.in_channels = in_channels
-        self.out_channels = out_channels
-        self.weight_s = nn.Parameter(torch.randn(out_channels, in_channels, kernel_size, kernel_size) * 0.01)
-        self.weight_x = nn.Parameter(torch.randn(out_channels, in_channels, kernel_size, kernel_size) * 0.01)
-        self.weight_y = nn.Parameter(torch.randn(out_channels, in_channels, kernel_size, kernel_size) * 0.01)
-        self.weight_b = nn.Parameter(torch.randn(out_channels, in_channels, kernel_size, kernel_size) * 0.01)
-        self.padding = padding
+        self.dim = dim // 4 
+        if self.dim * 4 != dim:
+            raise ValueError("Clifford channels must be divisible by 4")
+        self.conv_s = nn.Conv2d(self.dim, self.dim, kernel_size, padding=padding)
+        self.conv_x = nn.Conv2d(self.dim, self.dim, kernel_size, padding=padding)
+        self.conv_y = nn.Conv2d(self.dim, self.dim, kernel_size, padding=padding)
+        self.conv_b = nn.Conv2d(self.dim, self.dim, kernel_size, padding=padding)
 
     def forward(self, x):
-        s_in, x_in, y_in, b_in = torch.chunk(x, 4, dim=1)
-        s_out = (F.conv2d(s_in, self.weight_s, padding=self.padding) + 
-                 F.conv2d(x_in, self.weight_x, padding=self.padding) + 
-                 F.conv2d(y_in, self.weight_y, padding=self.padding) - 
-                 F.conv2d(b_in, self.weight_b, padding=self.padding))
-        x_out = (F.conv2d(s_in, self.weight_x, padding=self.padding) + 
-                 F.conv2d(x_in, self.weight_s, padding=self.padding) - 
-                 F.conv2d(y_in, self.weight_b, padding=self.padding) + 
-                 F.conv2d(b_in, self.weight_y, padding=self.padding))
-        y_out = (F.conv2d(s_in, self.weight_y, padding=self.padding) + 
-                 F.conv2d(x_in, self.weight_b, padding=self.padding) + 
-                 F.conv2d(y_in, self.weight_s, padding=self.padding) - 
-                 F.conv2d(b_in, self.weight_x, padding=self.padding))
-        b_out = (F.conv2d(s_in, self.weight_b, padding=self.padding) + 
-                 F.conv2d(x_in, self.weight_y, padding=self.padding) - 
-                 F.conv2d(y_in, self.weight_x, padding=self.padding) + 
-                 F.conv2d(b_in, self.weight_s, padding=self.padding))
-        return torch.cat([s_out, x_out, y_out, b_out], dim=1)
+        s, vx, vy, b = torch.chunk(x, 4, dim=1)
+        out_s = self.conv_s(s) - self.conv_x(vx) - self.conv_y(vy) - self.conv_b(b)
+        out_x = self.conv_x(s) + self.conv_s(vx) - self.conv_b(vy) + self.conv_y(b)
+        out_y = self.conv_y(s) + self.conv_b(vx) + self.conv_s(vy) - self.conv_x(b)
+        out_b = self.conv_b(s) + self.conv_y(vx) - self.conv_x(vy) + self.conv_s(b)
+        return torch.cat([out_s, out_x, out_y, out_b], dim=1)
 
-class StochasticHamiltonianPropagator(nn.Module):
+class StochasticHamiltonianSSM(nn.Module):
     def __init__(self, hidden_dim, h, w):
         super().__init__()
-        self.hidden_dim = hidden_dim
         self.freq_h = h
-        self.freq_w = w 
+        self.freq_w = w // 2 + 1
         self.hamiltonian_real = nn.Parameter(torch.randn(hidden_dim, self.freq_h, self.freq_w) * 0.01)
         self.hamiltonian_imag = nn.Parameter(torch.randn(hidden_dim, self.freq_h, self.freq_w) * 0.01)
-        self.noise_scale = nn.Parameter(torch.tensor(0.05))
+        self.noise_scale = nn.Parameter(torch.tensor(0.02))
 
     def forward(self, z, dt):
-        z_spec = torch.fft.fft2(z, norm='ortho')
+        z_spec = torch.fft.rfft2(z, norm='ortho')
         sigma = F.softplus(self.noise_scale)
         z_next_r, z_next_i = FusedHamiltonian.apply(
             z_spec.real, z_spec.imag, 
@@ -53,16 +209,16 @@ class StochasticHamiltonianPropagator(nn.Module):
             dt, sigma
         )
         z_shifted_spec = torch.complex(z_next_r, z_next_i)
-        z_shifted = torch.fft.ifft2(z_shifted_spec, s=(self.freq_h, self.freq_w), norm='ortho')
-        return z_shifted
+        z_next = torch.fft.irfft2(z_shifted_spec, s=(z.shape[2], z.shape[3]), norm='ortho')
+        return z_next
 
-class StreamFunctionConstraint(nn.Module):
+class StreamFunctionMixing(nn.Module):
     def __init__(self, in_ch, h, w):
         super().__init__()
         self.psi_net = nn.Sequential(
-            nn.Conv2d(in_ch, 64, 3, padding=1),
+            nn.Conv2d(in_ch, in_ch, 3, padding=1, groups=in_ch),
             nn.SiLU(),
-            nn.Conv2d(64, 1, 3, padding=1)
+            nn.Conv2d(in_ch, 1, 3, padding=1)
         )
         yy, xx = torch.meshgrid(torch.linspace(-1, 1, h), torch.linspace(-1, 1, w), indexing='ij')
         self.register_buffer('grid_base', torch.stack((xx, yy), dim=-1))
@@ -70,7 +226,7 @@ class StreamFunctionConstraint(nn.Module):
     def forward(self, z, dt):
         B, C, H, W = z.shape
         dt_view = dt.view(B, 1, 1, 1)
-        psi = self.psi_net(z.real) * dt_view
+        psi = self.psi_net(z) * dt_view
         if not self.training and z.is_cuda:
             u, v = fused_curl_2d(psi)
         else:
@@ -78,48 +234,12 @@ class StreamFunctionConstraint(nn.Module):
         flow_norm = torch.cat([u / (W/2), v / (H/2)], dim=1).permute(0, 2, 3, 1)
         grid = self.grid_base.unsqueeze(0).expand(B, -1, -1, -1)
         sampling_grid = grid - flow_norm
-        z_real = F.grid_sample(z.real, sampling_grid, align_corners=True, mode='bilinear', padding_mode='border')
-        z_imag = F.grid_sample(z.imag, sampling_grid, align_corners=True, mode='bilinear', padding_mode='border')
-        return torch.complex(z_real, z_imag)
+        z_out = F.grid_sample(z, sampling_grid, align_corners=True, mode='bilinear', padding_mode='border')
+        return z_out
 
     def _curl_pytorch(self, psi):
         psi_pad = F.pad(psi, (1, 1, 1, 1), mode='replicate')
         u = (psi_pad[:, :, 2:, 1:-1] - psi_pad[:, :, :-2, 1:-1]) / 2.0
         v = -(psi_pad[:, :, 1:-1, 2:] - psi_pad[:, :, 1:-1, :-2]) / 2.0
         return u, v
-
-class GeoSymSDE(nn.Module):
-    def __init__(self, in_ch, out_ch, hidden_dim=8, h=64, w=128):
-        super().__init__()
-        self.clifford_encoder = nn.Sequential(
-            nn.Conv2d(in_ch, hidden_dim * 4, 1),
-            CliffordConv2d(hidden_dim, hidden_dim, 3, padding=1),
-            nn.SiLU()
-        )
-        self.to_complex_real = nn.Conv2d(hidden_dim * 4, hidden_dim, 1)
-        self.to_complex_imag = nn.Conv2d(hidden_dim * 4, hidden_dim, 1)
-        self.stochastic_op = StochasticHamiltonianPropagator(hidden_dim, h, w)
-        self.stream_op = StreamFunctionConstraint(hidden_dim, h, w)
-        self.from_complex = nn.Conv2d(hidden_dim * 2, hidden_dim * 4, 1)
-        self.clifford_decoder = nn.Sequential(
-            CliffordConv2d(hidden_dim, hidden_dim, 3, padding=1),
-            nn.SiLU(),
-            nn.Conv2d(hidden_dim * 4, out_ch, 1)
-        )
-
-    def forward(self, x, dt):
-        geo_feat = self.clifford_encoder(x)
-        z_real = self.to_complex_real(geo_feat)
-        z_imag = self.to_complex_imag(geo_feat)
-        z = torch.complex(z_real, z_imag)
-        
-        dt_half = dt / 2.0
-        z_half = self.stochastic_op(z, dt_half)
-        z_adv = self.stream_op(z_half, dt)
-        z_final = self.stochastic_op(z_adv, dt_half)
-        
-        z_cat = torch.cat([z_final.real, z_final.imag], dim=1)
-        rec_feat = self.from_complex(z_cat)
-        out = self.clifford_decoder(rec_feat + geo_feat)
-        return x + out
 
