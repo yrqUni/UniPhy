ab9ffdd3a4a7470ad1cb7404868bcb9ffd0fa351
RuiqingYan <yrqUni@gmail.com>
2026-01-16 07:49:19 +0000

    uniphy

--- Model/UniPhy/CliffordConv2d.py
+++ Model/UniPhy/CliffordConv2d.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 class CliffordConv2d(nn.Module):
     def __init__(self, dim, kernel_size=3, padding=1):
@@ -7,16 +8,29 @@ class CliffordConv2d(nn.Module):
         self.dim = dim // 4
         if self.dim * 4 != dim:
             raise ValueError("Clifford channels must be divisible by 4")
-        self.conv_s = nn.Conv2d(self.dim, self.dim, kernel_size, padding=padding)
-        self.conv_x = nn.Conv2d(self.dim, self.dim, kernel_size, padding=padding)
-        self.conv_y = nn.Conv2d(self.dim, self.dim, kernel_size, padding=padding)
-        self.conv_b = nn.Conv2d(self.dim, self.dim, kernel_size, padding=padding)
+        
+        self.w_s = nn.Parameter(torch.empty(self.dim, self.dim, kernel_size, kernel_size))
+        self.w_x = nn.Parameter(torch.empty(self.dim, self.dim, kernel_size, kernel_size))
+        self.w_y = nn.Parameter(torch.empty(self.dim, self.dim, kernel_size, kernel_size))
+        self.w_b = nn.Parameter(torch.empty(self.dim, self.dim, kernel_size, kernel_size))
+        
+        self.bias = nn.Parameter(torch.zeros(dim))
+        
+        nn.init.kaiming_normal_(self.w_s)
+        nn.init.kaiming_normal_(self.w_x)
+        nn.init.kaiming_normal_(self.w_y)
+        nn.init.kaiming_normal_(self.w_b)
+        
+        self.kernel_size = kernel_size
+        self.padding = padding
 
     def forward(self, x):
-        s, vx, vy, b = torch.chunk(x, 4, dim=1)
-        out_s = self.conv_s(s) - self.conv_x(vx) - self.conv_y(vy) - self.conv_b(b)
-        out_x = self.conv_x(s) + self.conv_s(vx) - self.conv_b(vy) + self.conv_y(b)
-        out_y = self.conv_y(s) + self.conv_b(vx) + self.conv_s(vy) - self.conv_x(b)
-        out_b = self.conv_b(s) + self.conv_y(vx) - self.conv_x(vy) + self.conv_s(b)
-        return torch.cat([out_s, out_x, out_y, out_b], dim=1)
+        row_s = torch.cat([self.w_s, -self.w_x, -self.w_y, -self.w_b], dim=1)
+        row_x = torch.cat([self.w_x,  self.w_s, -self.w_b,  self.w_y], dim=1)
+        row_y = torch.cat([self.w_y,  self.w_b,  self.w_s, -self.w_x], dim=1)
+        row_b = torch.cat([self.w_b,  self.w_y, -self.w_x,  self.w_s], dim=1)
+        
+        W_fused = torch.cat([row_s, row_x, row_y, row_b], dim=0)
+        
+        return F.conv2d(x, W_fused, bias=self.bias, padding=self.padding)
 
--- Model/UniPhy/SpectralStep.py
+++ Model/UniPhy/SpectralStep.py
@@ -18,24 +18,33 @@ class SpectralStep(nn.Module):
     def forward(self, x, dt):
         B, C, H, W = x.shape
         x_spec = torch.fft.rfft2(x, norm="ortho")
+        
         params = self.estimator(x)
         nu, theta = torch.chunk(params, 2, dim=1)
         nu = F.softplus(nu)
-        theta = (torch.tanh(theta) * math.pi)
+        theta = torch.tanh(theta) * math.pi
+        
         if dt.dim() < nu.dim():
             dt_view = dt.view(B, 1, 1, 1)
         else:
             dt_view = dt
+            
         decay = torch.exp(-nu * dt_view)
         angle = theta * dt_view
+        
         operator = torch.complex(decay * torch.cos(angle), decay * torch.sin(angle))
+        
+        operator_sum = operator.sum(dim=1, keepdim=True)
+        
         feat_spec = x_spec[:, :, :, :self.w_freq]
-        if operator.shape[2] == 1 and feat_spec.shape[2] > 1:
-            feat_spec = feat_spec * operator
+        
+        if operator_sum.shape[2] == 1:
+             feat_spec = feat_spec * operator_sum
         else:
-            feat_spec = feat_spec.unsqueeze(1) * operator.unsqueeze(2)
-            feat_spec = feat_spec.sum(dim=1)
+             feat_spec = feat_spec * operator_sum
+             
         x_out_spec = x_spec.clone()
         x_out_spec[:, :, :, :self.w_freq] = feat_spec
+        
         return torch.fft.irfft2(x_out_spec, s=(H, W), norm="ortho")
 
