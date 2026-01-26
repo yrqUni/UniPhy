78838878a365dd9d4fd8d97cc4560ef9461840af
ruiqing.yan <ruiqing.yan@unilabs.org.cn>
2026-01-27 01:44:32 +0800

    UniPhy

--- Model/UniPhy/PScan.py
+++ Model/UniPhy/PScan.py
@@ -1,12 +1,8 @@
 import torch
 import torch.nn.functional as F
 
-try:
-    import triton
-    import triton.language as tl
-    HAS_TRITON = True
-except ImportError:
-    HAS_TRITON = False
+import triton
+import triton.language as tl
 
 
 def next_power_of_2(n):
@@ -14,113 +10,111 @@ def next_power_of_2(n):
         return 1
     return 1 << (n - 1).bit_length()
 
+@triton.jit
+def complex_mul(ar, ai, br, bi):
+    return ar * br - ai * bi, ar * bi + ai * br
 
-if HAS_TRITON:
-    @triton.jit
-    def complex_mul(ar, ai, br, bi):
-        return ar * br - ai * bi, ar * bi + ai * br
+@triton.jit
+def mat2x2_scan_combine(
+    a00r, a00i, a01r, a01i, a10r, a10i, a11r, a11i,
+    x00r, x00i, x01r, x01i, x10r, x10i, x11r, x11i,
+    b00r, b00i, b01r, b01i, b10r, b10i, b11r, b11i,
+    y00r, y00i, y01r, y01i, y10r, y10i, y11r, y11i,
+):
+    c00r, c00i = complex_mul(b00r, b00i, a00r, a00i)
+    t1r, t1i = complex_mul(b01r, b01i, a10r, a10i)
+    c00r, c00i = c00r + t1r, c00i + t1i
 
-    @triton.jit
-    def mat2x2_scan_combine(
-        a00r, a00i, a01r, a01i, a10r, a10i, a11r, a11i,
-        x00r, x00i, x01r, x01i, x10r, x10i, x11r, x11i,
-        b00r, b00i, b01r, b01i, b10r, b10i, b11r, b11i,
-        y00r, y00i, y01r, y01i, y10r, y10i, y11r, y11i,
-    ):
-        c00r, c00i = complex_mul(b00r, b00i, a00r, a00i)
-        t1r, t1i = complex_mul(b01r, b01i, a10r, a10i)
-        c00r, c00i = c00r + t1r, c00i + t1i
+    c01r, c01i = complex_mul(b00r, b00i, a01r, a01i)
+    t2r, t2i = complex_mul(b01r, b01i, a11r, a11i)
+    c01r, c01i = c01r + t2r, c01i + t2i
 
-        c01r, c01i = complex_mul(b00r, b00i, a01r, a01i)
-        t2r, t2i = complex_mul(b01r, b01i, a11r, a11i)
-        c01r, c01i = c01r + t2r, c01i + t2i
+    c10r, c10i = complex_mul(b10r, b10i, a00r, a00i)
+    t3r, t3i = complex_mul(b11r, b11i, a10r, a10i)
+    c10r, c10i = c10r + t3r, c10i + t3i
 
-        c10r, c10i = complex_mul(b10r, b10i, a00r, a00i)
-        t3r, t3i = complex_mul(b11r, b11i, a10r, a10i)
-        c10r, c10i = c10r + t3r, c10i + t3i
+    c11r, c11i = complex_mul(b10r, b10i, a01r, a01i)
+    t4r, t4i = complex_mul(b11r, b11i, a11r, a11i)
+    c11r, c11i = c11r + t4r, c11i + t4i
 
-        c11r, c11i = complex_mul(b10r, b10i, a01r, a01i)
-        t4r, t4i = complex_mul(b11r, b11i, a11r, a11i)
-        c11r, c11i = c11r + t4r, c11i + t4i
+    bx00r, bx00i = complex_mul(b00r, b00i, x00r, x00i)
+    bx01r, bx01i = complex_mul(b01r, b01i, x10r, x10i)
+    z00r, z00i = bx00r + bx01r + y00r, bx00i + bx01i + y00i
 
-        bx00r, bx00i = complex_mul(b00r, b00i, x00r, x00i)
-        bx01r, bx01i = complex_mul(b01r, b01i, x10r, x10i)
-        z00r, z00i = bx00r + bx01r + y00r, bx00i + bx01i + y00i
+    bx02r, bx02i = complex_mul(b00r, b00i, x01r, x01i)
+    bx03r, bx03i = complex_mul(b01r, b01i, x11r, x11i)
+    z01r, z01i = bx02r + bx03r + y01r, bx02i + bx03i + y01i
 
-        bx02r, bx02i = complex_mul(b00r, b00i, x01r, x01i)
-        bx03r, bx03i = complex_mul(b01r, b01i, x11r, x11i)
-        z01r, z01i = bx02r + bx03r + y01r, bx02i + bx03i + y01i
+    bx10r, bx10i = complex_mul(b10r, b10i, x00r, x00i)
+    bx11r, bx11i = complex_mul(b11r, b11i, x10r, x10i)
+    z10r, z10i = bx10r + bx11r + y10r, bx10i + bx11i + y10i
 
-        bx10r, bx10i = complex_mul(b10r, b10i, x00r, x00i)
-        bx11r, bx11i = complex_mul(b11r, b11i, x10r, x10i)
-        z10r, z10i = bx10r + bx11r + y10r, bx10i + bx11i + y10i
+    bx12r, bx12i = complex_mul(b10r, b10i, x01r, x01i)
+    bx13r, bx13i = complex_mul(b11r, b11i, x11r, x11i)
+    z11r, z11i = bx12r + bx13r + y11r, bx12i + bx13i + y11i
 
-        bx12r, bx12i = complex_mul(b10r, b10i, x01r, x01i)
-        bx13r, bx13i = complex_mul(b11r, b11i, x11r, x11i)
-        z11r, z11i = bx12r + bx13r + y11r, bx12i + bx13i + y11i
-
-        return (
-            c00r, c00i, c01r, c01i, c10r, c10i, c11r, c11i,
-            z00r, z00i, z01r, z01i, z10r, z10i, z11r, z11i,
-        )
-
-    @triton.autotune(
-        configs=[
-            triton.Config({}, num_warps=2),
-            triton.Config({}, num_warps=4),
-            triton.Config({}, num_warps=8),
-        ],
-        key=["L"],
+    return (
+        c00r, c00i, c01r, c01i, c10r, c10i, c11r, c11i,
+        z00r, z00i, z01r, z01i, z10r, z10i, z11r, z11i,
     )
-    @triton.jit
-    def mat2x2_pscan_kernel(
-        A_ptr, X_ptr, Y_ptr,
-        stride_a_batch, stride_a_time, stride_a_d1, stride_a_d2,
-        stride_x_batch, stride_x_time, stride_x_d1, stride_x_d2,
-        L, BLOCK_SIZE: tl.constexpr, REVERSE: tl.constexpr,
-    ):
-        pid = tl.program_id(axis=0)
-        A_base = A_ptr + pid * stride_a_batch
-        X_base = X_ptr + pid * stride_x_batch
-        Y_base = Y_ptr + pid * stride_x_batch
 
-        offs = tl.arange(0, BLOCK_SIZE)
-        mask = offs < L
-        read_offs = L - 1 - offs if REVERSE else offs
+@triton.autotune(
+    configs=[
+        triton.Config({}, num_warps=2),
+        triton.Config({}, num_warps=4),
+        triton.Config({}, num_warps=8),
+    ],
+    key=["L"],
+)
+@triton.jit
+def mat2x2_pscan_kernel(
+    A_ptr, X_ptr, Y_ptr,
+    stride_a_batch, stride_a_time, stride_a_d1, stride_a_d2,
+    stride_x_batch, stride_x_time, stride_x_d1, stride_x_d2,
+    L, BLOCK_SIZE: tl.constexpr, REVERSE: tl.constexpr,
+):
+    pid = tl.program_id(axis=0)
+    A_base = A_ptr + pid * stride_a_batch
+    X_base = X_ptr + pid * stride_x_batch
+    Y_base = Y_ptr + pid * stride_x_batch
 
-        a00r = tl.load(A_base + read_offs * stride_a_time + 0, mask=mask, other=1.0)
-        a00i = tl.load(A_base + read_offs * stride_a_time + 1, mask=mask, other=0.0)
-        a01r = tl.load(A_base + read_offs * stride_a_time + stride_a_d2 + 0, mask=mask, other=0.0)
-        a01i = tl.load(A_base + read_offs * stride_a_time + stride_a_d2 + 1, mask=mask, other=0.0)
-        a10r = tl.load(A_base + read_offs * stride_a_time + stride_a_d1 + 0, mask=mask, other=0.0)
-        a10i = tl.load(A_base + read_offs * stride_a_time + stride_a_d1 + 1, mask=mask, other=0.0)
-        a11r = tl.load(A_base + read_offs * stride_a_time + stride_a_d1 + stride_a_d2 + 0, mask=mask, other=1.0)
-        a11i = tl.load(A_base + read_offs * stride_a_time + stride_a_d1 + stride_a_d2 + 1, mask=mask, other=0.0)
+    offs = tl.arange(0, BLOCK_SIZE)
+    mask = offs < L
+    read_offs = L - 1 - offs if REVERSE else offs
 
-        x00r = tl.load(X_base + read_offs * stride_x_time + 0, mask=mask, other=0.0)
-        x00i = tl.load(X_base + read_offs * stride_x_time + 1, mask=mask, other=0.0)
-        x01r = tl.load(X_base + read_offs * stride_x_time + stride_x_d2 + 0, mask=mask, other=0.0)
-        x01i = tl.load(X_base + read_offs * stride_x_time + stride_x_d2 + 1, mask=mask, other=0.0)
-        x10r = tl.load(X_base + read_offs * stride_x_time + stride_x_d1 + 0, mask=mask, other=0.0)
-        x10i = tl.load(X_base + read_offs * stride_x_time + stride_x_d1 + 1, mask=mask, other=0.0)
-        x11r = tl.load(X_base + read_offs * stride_x_time + stride_x_d1 + stride_x_d2 + 0, mask=mask, other=0.0)
-        x11i = tl.load(X_base + read_offs * stride_x_time + stride_x_d1 + stride_x_d2 + 1, mask=mask, other=0.0)
+    a00r = tl.load(A_base + read_offs * stride_a_time + 0, mask=mask, other=1.0)
+    a00i = tl.load(A_base + read_offs * stride_a_time + 1, mask=mask, other=0.0)
+    a01r = tl.load(A_base + read_offs * stride_a_time + stride_a_d2 + 0, mask=mask, other=0.0)
+    a01i = tl.load(A_base + read_offs * stride_a_time + stride_a_d2 + 1, mask=mask, other=0.0)
+    a10r = tl.load(A_base + read_offs * stride_a_time + stride_a_d1 + 0, mask=mask, other=0.0)
+    a10i = tl.load(A_base + read_offs * stride_a_time + stride_a_d1 + 1, mask=mask, other=0.0)
+    a11r = tl.load(A_base + read_offs * stride_a_time + stride_a_d1 + stride_a_d2 + 0, mask=mask, other=1.0)
+    a11i = tl.load(A_base + read_offs * stride_a_time + stride_a_d1 + stride_a_d2 + 1, mask=mask, other=0.0)
 
-        (_, _, _, _, _, _, _, _,
-         y00r, y00i, y01r, y01i, y10r, y10i, y11r, y11i) = tl.associative_scan(
-            (a00r, a00i, a01r, a01i, a10r, a10i, a11r, a11i,
-             x00r, x00i, x01r, x01i, x10r, x10i, x11r, x11i),
-            axis=0, combine_fn=mat2x2_scan_combine,
-        )
+    x00r = tl.load(X_base + read_offs * stride_x_time + 0, mask=mask, other=0.0)
+    x00i = tl.load(X_base + read_offs * stride_x_time + 1, mask=mask, other=0.0)
+    x01r = tl.load(X_base + read_offs * stride_x_time + stride_x_d2 + 0, mask=mask, other=0.0)
+    x01i = tl.load(X_base + read_offs * stride_x_time + stride_x_d2 + 1, mask=mask, other=0.0)
+    x10r = tl.load(X_base + read_offs * stride_x_time + stride_x_d1 + 0, mask=mask, other=0.0)
+    x10i = tl.load(X_base + read_offs * stride_x_time + stride_x_d1 + 1, mask=mask, other=0.0)
+    x11r = tl.load(X_base + read_offs * stride_x_time + stride_x_d1 + stride_x_d2 + 0, mask=mask, other=0.0)
+    x11i = tl.load(X_base + read_offs * stride_x_time + stride_x_d1 + stride_x_d2 + 1, mask=mask, other=0.0)
 
-        tl.store(Y_base + read_offs * stride_x_time + 0, y00r, mask=mask)
-        tl.store(Y_base + read_offs * stride_x_time + 1, y00i, mask=mask)
-        tl.store(Y_base + read_offs * stride_x_time + stride_x_d2 + 0, y01r, mask=mask)
-        tl.store(Y_base + read_offs * stride_x_time + stride_x_d2 + 1, y01i, mask=mask)
-        tl.store(Y_base + read_offs * stride_x_time + stride_x_d1 + 0, y10r, mask=mask)
-        tl.store(Y_base + read_offs * stride_x_time + stride_x_d1 + 1, y10i, mask=mask)
-        tl.store(Y_base + read_offs * stride_x_time + stride_x_d1 + stride_x_d2 + 0, y11r, mask=mask)
-        tl.store(Y_base + read_offs * stride_x_time + stride_x_d1 + stride_x_d2 + 1, y11i, mask=mask)
+    (_, _, _, _, _, _, _, _,
+        y00r, y00i, y01r, y01i, y10r, y10i, y11r, y11i) = tl.associative_scan(
+        (a00r, a00i, a01r, a01i, a10r, a10i, a11r, a11i,
+            x00r, x00i, x01r, x01i, x10r, x10i, x11r, x11i),
+        axis=0, combine_fn=mat2x2_scan_combine,
+    )
+
+    tl.store(Y_base + read_offs * stride_x_time + 0, y00r, mask=mask)
+    tl.store(Y_base + read_offs * stride_x_time + 1, y00i, mask=mask)
+    tl.store(Y_base + read_offs * stride_x_time + stride_x_d2 + 0, y01r, mask=mask)
+    tl.store(Y_base + read_offs * stride_x_time + stride_x_d2 + 1, y01i, mask=mask)
+    tl.store(Y_base + read_offs * stride_x_time + stride_x_d1 + 0, y10r, mask=mask)
+    tl.store(Y_base + read_offs * stride_x_time + stride_x_d1 + 1, y10i, mask=mask)
+    tl.store(Y_base + read_offs * stride_x_time + stride_x_d1 + stride_x_d2 + 0, y11r, mask=mask)
+    tl.store(Y_base + read_offs * stride_x_time + stride_x_d1 + stride_x_d2 + 1, y11i, mask=mask)
 
 
 def run_mat2x2_pscan_torch(A_complex, X_complex, reverse=False):
@@ -137,21 +131,15 @@ def run_mat2x2_pscan_torch(A_complex, X_complex, reverse=False):
 
 
 def run_mat2x2_pscan(A_real, X_real, L, reverse=False):
-    if HAS_TRITON and A_real.is_cuda:
-        Y_real = torch.empty_like(X_real)
-        BLOCK_SIZE = max(16, next_power_of_2(L))
-        mat2x2_pscan_kernel[(A_real.shape[0],)](
-            A_real, X_real, Y_real,
-            A_real.stride(0), A_real.stride(1), A_real.stride(2), A_real.stride(3),
-            X_real.stride(0), X_real.stride(1), X_real.stride(2), X_real.stride(3),
-            L, BLOCK_SIZE, reverse,
-        )
-        return Y_real
-
-    A_complex = torch.view_as_complex(A_real.contiguous())
-    X_complex = torch.view_as_complex(X_real.contiguous())
-    Y_complex = run_mat2x2_pscan_torch(A_complex, X_complex, reverse)
-    return torch.view_as_real(Y_complex).contiguous()
+    Y_real = torch.empty_like(X_real)
+    BLOCK_SIZE = max(16, next_power_of_2(L))
+    mat2x2_pscan_kernel[(A_real.shape[0],)](
+        A_real, X_real, Y_real,
+        A_real.stride(0), A_real.stride(1), A_real.stride(2), A_real.stride(3),
+        X_real.stride(0), X_real.stride(1), X_real.stride(2), X_real.stride(3),
+        L, BLOCK_SIZE, reverse,
+    )
+    return Y_real
 
 
 def expand_diag_to_matrix(A_diag, D):
